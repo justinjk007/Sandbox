cde917c6dceb492ef3c0da476d0a760e4e119c9b
Justin K -Linux <justinjoseph0007@gmail.com>
2017-02-04 08:50:47 -0500

    fixed file names and moved test into same file..

--- Data Structures/sort.py
+++ Data Structures/sort.py
@@ -14,5 +14,13 @@ class Sort(object):
                 self.data[j+1] = self.data[j]
                 j = j-1
             self.data[j+1] = cur
-
         return self.data
+
+
+# def tester():
+print "Testing started"
+unsorted = [54,545,6,547,547,87,547,0,-1,5,5,-78,-87,546]
+obj = Sort(unsorted)
+sortedData = obj.insertionSort()
+for val in sortedData:
+    print (str(val), ",")
--- Data Structures/test.py
+++ Data Structures/test.py
@@ -1,8 +1,5 @@
-from sort import Sort
-
-
 def tester():
-    print "Testinng started"
+    print "Testing started"
     unsorted = [54,545,6,547,547,87,547,0,-1,5,5,-78,-87,546]
     obj = Sort(unsorted)
     sortedData = obj.insertionSort()
