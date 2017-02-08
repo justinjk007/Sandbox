f87545a2d6ef45fab1634b9e933dded20e8bdfab
Justin K -Linux <justinjoseph0007@gmail.com>
2017-02-08 15:31:57 -0500

    added count and swap varibles

--- Data Structures/sort.py
+++ Data Structures/sort.py
@@ -5,22 +5,34 @@ class Sort(object):
 
     def insertionSort(self):
         print "Sorting started"
+        global counter
+        global swap
+        counter = 0
+        swap = 0
         size = len(self.data)
         for i in range(0, size):
-            cur = self.data[i]
+            CurrentItem = self.data[i]
+            # j is the divider of the sorted and unsorted portion
             j = i-1
-            while j >= 0 and self.data[j] > cur:
-                self.data[j+1] = self.data[j]
-                j = j-1
-            self.data[j+1] = cur
-        return self.data
+            print j
+            while j >= 0 and self.data[j] > CurrentItem:
+                counter += 1
+                self.data[j+1] = self.data[j]  # Swap happens here.
+                swap += 1
+                j = j-1  # incrementing the divider because we swapped values
+            self.data[j+1] = CurrentItem
+        return self.data  # Method ends here
+# Class ends here
 
 
 # def tester -> Tests the above code
 print "Testing started"
-unsorted = [54,545,6,547,547,87,547,0,-1,5,5,-78,-87,546]
+unsorted = [547, 547, 547, 546, 545, 87, 54, 6, 5, 5, 0, -1, -78, -87]
 obj = Sort(unsorted)
 sortedData = obj.insertionSort()
+print ("Lenth of the array is: "+str(len(sortedData)))
+print ("Number of comaprisons: "+str(counter))
+print ("Number of swaps: "+str(swap))
 for val in sortedData:
     print (str(val) + ","),
 # The comma on the end of the line 26 makes the printing at the same line.
