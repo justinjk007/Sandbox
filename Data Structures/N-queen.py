47b54770263b0f2bb36d5172c5b9bf8388bb0262
Justin K -Linux <justinjoseph0007@gmail.com>
2017-02-17 13:55:08 -0500

    well...

--- Data Structures/N-queen.py
+++ Data Structures/N-queen.py
@@ -1,9 +1,10 @@
 '''
+Assignment #1
 Author  : Justin Kaipada
 ID      : 100590167
 Date    : 17-02-2017
-Purpose : This program solves the N-queen problem in the recurive ans the 
-          iterative way.
+Purpose : This program solves the N-queen problem in the recurive and the
+          iterative way. Backtracking algorithm has been implmented here
 '''
 
 
@@ -15,3 +16,26 @@ def iterativeMethod():
 def recursiveMethod():
     "This solves the N-queen problem in the recursive way"
     return 0
+
+
+def draw():
+    "This method is used to draw the solved problem into human readable way"
+    print "\n"
+    print "The Q represents the queens"
+    print "---------------------------------------"
+    for i in range(0, N):
+        print "|",
+        for j in range(0, N):
+            print board[i][j] + "|",
+        print "\n"
+    print "---------------------------------------"
+
+
+# N = input("Please enter the value of N: ")
+# print N
+N = 5
+board = []
+for i in range(0, N):
+    board.append(["O "]*N)
+
+draw()
