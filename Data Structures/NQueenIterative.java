cd84270802b94dedc2f68dfbc192c61b4d8aa548
Justin K -Linux <justinjoseph0007@gmail.com>
2017-02-20 09:41:20 -0500

    Works for every number greater than $, now...Awesome

--- Data Structures/NQueenIterative.java
+++ Data Structures/NQueenIterative.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class NQueenIterative
 {
-    static int N = 4;
+    static int N = 9;
 
     static boolean iterativeSolution(int board[][]) 
     {
@@ -26,80 +26,95 @@ public class NQueenIterative
                                 coloumn++;
                                 row = 0;
                                 System.out.println("Queen Placed");
+                                System.out.println("Queencount now is "+queenCount);
                                 if(queenCount == N)
                                     return false; // We have a solution
                             }
-			else if(row < N-1)
-			    row++;
-			else if(row == N)
-			    {
-				if(!lastRow.empty() && !lastCol.empty())
-				    {
-					Integer temp1 = (Integer) lastRow.pop();
-					Integer temp2 = (Integer) lastCol.pop();
-					coloumn = temp2;
-					if(row < N-1) 
-					    row = temp1+1;
-					else
-					    row = 0;
-					System.out.println(row-1+" "+coloumn);
-					System.out.println("Queen Removed");
-					queenCount--;
-					board[temp1][temp2] = 0;
-				    }
-				else
-				    System.out.println("Unimaginable error");
-			    }
-			else // When row = N
-			    row = 0;
-		    }
-	    }
-	return true; // This statement is returned, when no solutions are found
+                        else if(row < N-1)
+                            row++;
+                        else if(row == N-1)
+                            {
+                                System.out.println("Backtracking");
+                                if(!lastRow.empty() && !lastCol.empty())
+                                    {
+                                        Integer temp1 = (Integer) lastRow.pop();
+                                        Integer temp2 = (Integer) lastCol.pop();
+                                        System.out.println(temp1+" "+temp2);
+                                        board[temp1][temp2] = 0;
+                                        queenCount--;
+                                        System.out.println("Queen Removed");
+                                        coloumn = temp2;
+                                        row = temp1;
+                                        if(row < N-1) 
+                                            row = temp1+1;
+                                        else if(row == N-1)
+                                            {
+                                                temp1 = (Integer) lastRow.pop();
+                                                temp2 = (Integer) lastCol.pop();
+                                                System.out.println(temp1+" "+temp2);
+                                                board[temp1][temp2] = 0;
+                                                System.out.println("Queen Removed");
+                                                coloumn = temp2;
+                                                row = temp1+1;
+                                                queenCount--;
+                                                break;
+                                            }                                            
+                                        else
+                                            System.out.println("This is impossible");
+                                    }
+                                else
+                                    System.out.println("Unimaginable error");
+                            }
+                        else
+                            System.out.println("How does it even get here");
+                    }
+            }
+        return true; // This statement is returned, when no solutions are found
     } 
 
     static void drawLayout(int board[][])
     {
-	for (int i = 0; i < N; i++)
-	    {
-		for (int j = 0; j < N; j++)
-		    if (board[i][j] == 1)
-			System.out.print("Q   ");
-		    else
-			System.out.print("_   ");
-		System.out.println("\n");
-	    }
+        for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    if (board[i][j] == 1)
+                        System.out.print("Q   ");
+                    else
+                        System.out.print("_   ");
+                System.out.println("\n");
+            }
     }
 
     static boolean conflictCheck(int board[][], int row, int coloumn)
     {
-	int i, j;
-	for (i = 0; i < coloumn; i++) {
-	    if (board[row][i] == 1)
-		return false; // Returns false if there is a conflict
-	}
-	for (i = row, j = coloumn; i >= 0 && j >= 0; i--, j--) {
-	    if (board[i][j] == 1)
-		return false;
-	}
-	for (i = row, j = coloumn; j >= 0 && i < N; i++, j--) {
-	    if (board[i][j] == 1)
-		return false;
-	}
-	return true; //No conflicts are occuring, queen can be placed
+        int i, j;
+        for (i = 0; i < coloumn; i++) {
+            if (board[row][i] == 1)
+                return false; // Returns false if there is a conflict
+        }
+        for (i = row, j = coloumn; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 1)
+                return false;
+        }
+        for (i = row, j = coloumn; j >= 0 && i < N; i++, j--) {
+            if (board[i][j] == 1)
+                return false;
+        }
+        return true; //No conflicts are occuring, queen can be placed
     }
 
     
     public static void main(String[] args)
     {
-	System.out.println("N-queen solution calculated for N="+N);
-	System.out.println("");
-	int[][] board = new int[N][N];
+        System.out.println("N-queen solution calculated for N="+N);
+        System.out.println("");
+        int[][] board = new int[N][N];
 
-	if (iterativeSolution(board)) { 
-	    System.out.println("Solution not found.");
-	}
-	else
-	    drawLayout(board);
+        if (iterativeSolution(board)) { 
+            System.out.println("Solution not found.");
+        }
+        else
+            drawLayout(board);
 
     } // End of main method
 }
