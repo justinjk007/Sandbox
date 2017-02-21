e59f921b45f8b2e72678970ae7653b14dbb2b4d3
Justin K -Windows <justinjoseph0007@gmail.com>
2017-02-21 08:45:16 -0500

    This is only a test commit

--- Data Structures/for.py
+++ Data Structures/for.py
@@ -1,2 +1,2 @@
-for x in range(1, 10):
+for x in range(10):
     print x
---
+++ Data Structures/maxIndex.py
@@ -0,0 +1,7 @@
+distance = [0,2,2,3,5,6,6]
+
+maxIndex = distance.index(max(distance))
+
+print distance
+print "The biggest element is "+str(distance[maxIndex])
+print "The index of the biggest element is " + str(maxIndex)
