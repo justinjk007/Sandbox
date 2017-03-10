706dd3a92ee344614124d14d7cc9c6b1f553b9f1
Justin K -Linux <justinjoseph0007@gmail.com>
2017-03-10 14:57:48 -0500

    Added main execution command

--- src/BinaryTreeTraverse/traverse.py
+++ src/BinaryTreeTraverse/traverse.py
@@ -1,15 +1,22 @@
-def main():
-    "This is the main method and the beginning of the program"
-    #TODO
-
 def preOrder():
     "This method returns the pre-order traversal of the binary tree"
     #TODO
+
 
 def postOrder():
     "This method returns the post-order traversal of the binary tree"
     #TODO
 
+
 def inOrder():
     "This method returns the in-order traversal of the binary tree"
     #TODO
+
+
+def main():
+    "This is the main method and the beginning of the program"
+    #TODO
+
+
+if __name__ == "__main__":
+    main()
