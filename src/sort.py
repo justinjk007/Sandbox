4fef91a5bc38a789c0a02e34267b10acc34cce1e
Justin K -W10 <justinjoseph0007@gmail.com>
2017-03-10 23:13:27 -0500

    Gotta implement heap sort

--- src/sort.py
+++ src/sort.py
@@ -1,42 +1,43 @@
-class Sort(object):
+import collections
+compare = lambda x,y: collections.Counter(x) == collections.Counter(y)
 
-    def __init__(self, unsortedData=[]):
-        self.data = unsortedData
-
-    def insertionSort(self):
-        print "\nSorting started"
-        global counter
-        global swap
-        counter = 0
-        swap = 0
-        size = len(self.data)
-        for i in range(0, size):
-            CurrentItem = self.data[i]
-            # j is the divider of the sorted and unsorted portion
-            j = i-1
-            while j >= 0 and self.data[j] > CurrentItem:
-                counter += 1
-                self.data[j+1] = self.data[j]  # Swap happens here.
-                swap += 1
-                j = j-1  # incrementing the divider because we swapped values
-            self.data[j+1] = CurrentItem
-        return self.data  # Method ends here
-# Class ends here
+def insertionSort(data):
+    "Implements Insertion sort"
+    size = len(data)
+    for i in range(0, size):
+        CurrentItem = data[i]
+        # j is the divider of the sorted and unsorted portion
+        j = i-1
+        while j >= 0 and data[j] > CurrentItem:
+            data[j+1] = data[j]  # Swap happens here.
+            j = j-1  # incrementing the divider because we swapped values
+            data[j+1] = CurrentItem
+        return data  # Method ends here
+    
+def HeapSort(data):
+    "Implements Heapsort"
+    #TODO
 
 
-# def tester -> Tests the above code
-print "Testing started"
-unsorted = [54, 587, 5, -545, 545, 47, 2, -54, 0, 0, 1, 2]
-print("Unsorted array: "),
-for val in unsorted:
-    print(str(val) + ","),
-obj = Sort(unsorted)
-sortedData = obj.insertionSort()
-print("Lenth of the array is: "+str(len(sortedData)))
-print("Number of comaprisons: "+str(counter))
-print("Number of swaps: "+str(swap))
-print("Sorted array: "),
-for val in sortedData:
-    print(str(val) + ","),
-# The comma on the end of the line 26 makes the printing at the same line.
-print "\nTesting completed"
+def main():
+    "This is the main method and the beginning of the program"
+    print ""
+    unsorted = [54, 587, 5, -545, 545, 47, 2, -54, 0, 0, 1, 2]
+    print("Unsorted array: "),
+    for val in unsorted:
+        print(str(val) + ","),
+    sortedData = insertionSort(unsorted)
+    print("\n ======> Lenth of the array is: "+str(len(sortedData)))
+    print("Sorted array: "),
+    for val in sortedData:
+        print(str(val) + ","),
+        # The comma on the end of the line 26 makes the printing at the same line
+
+    if(compare(sortedData, [54, 587, 5, -545, 545, 47, 2, -54, 0, 0, 1, 2])):
+        print "\n ======> Sorting was successful"
+    else:
+        print "\n ======> SORTING FAILED!!"
+
+
+if __name__ == "__main__":
+    main()
