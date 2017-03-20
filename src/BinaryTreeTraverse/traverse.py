022ca6e6a620975c3047797ff2f8b94805e22069
Justin K -Linux <justinjoseph0007@gmail.com>
2017-03-20 13:26:16 -0400

    Implemeted all orders from a tree type array

--- src/BinaryTreeTraverse/traverse.py
+++ src/BinaryTreeTraverse/traverse.py
@@ -5,35 +5,46 @@ import math
 # you only have to write four methods, post to pre, in to pre, pre to
 # post, pre to in
 
-# Unless we assume we have a complete binary tree we have no way of approaching this
+# Assuming we have a complete binary tree
 
 
-def detect(data):
-    def postToPre(data):
-        pass
+# def detect(data, order):
+#     def postToPre(data):
+#         pass
 
-    def inToPre(data):
-        pass
+#     def inToPre(data):
+#         pass
 
-    if order == 'post-order':
-        postToPre(data)
-    elif order == 'in-order':
-        inToPre(data)
-    else:
-        print "Unexpected inputs"
+#     if order == 'post-order':
+#         postToPre(data)
+#     elif order == 'in-order':
+#         inToPre(data)
+#     else:
+#         print "Unexpected inputs"
 
-def preOrder(data):
+
+def preOrder(data, node):
     "This method returns the pre-order traversal of the binary tree"
-    pass
+    if node <= len(data) - 1:
+        print data[node],
+        preOrder(data, left(node))
+        preOrder(data, right(node))
 
 
-def postOrder(data):
+def postOrder(data, node):
     "This method returns the post-order traversal of the binary tree"
+    if node <= len(data) - 1:
+        postOrder(data, left(node))
+        postOrder(data, right(node))
+        print data[node],
 
 
-def inOrder(data):
+def inOrder(data, node):
     "This method returns the in-order traversal of the binary tree"
-    #TODO
+    if node <= len(data) - 1:
+        inOrder(data, left(node))
+        print data[node],
+        inOrder(data, right(node))
 
 
 def parent(i):
@@ -57,23 +68,37 @@ def swap(data, i, j):
     return data
 
 
-def main(data, order):
-    "This is the main method and the beginning of the program"
+# def main(data, order):
+#     "This is the main method and the beginning of the program"
+#     null = None
+#     data = [null] + data  # Make the given array into a workable tree
+#     if order == "pre-order":
+#         inOrder(data)
+#         postOrder(data)
+#     elif order == "post-order":
+#         inOrder(data)
+#         preOrder(data)
+#     elif order == "in-order":
+#         postOrder(data)
+#         preOrder(data)
+#     else:
+#         print "Incorrect inputs.."
+
+# testList = [1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12]
+# if __name__ == "__main__":
+#     main(testList, "pre-order")
+
+def main():
+    print ""
+    testList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    testList2 = [1, 2, 9, 3, 6, 10, 12, 4, 5, 7, 8, 11]
+    testList3 = [1, 2, 3, 4, 5]
     null = None
-    data = [null] + data  # Make the given array into a workable tree
-    if order == "pre-order":
-        inOrder(data)
-        postOrder(data)
-    elif order == "post-order":
-        inOrder(data)
-        preOrder(data)
-    elif order == "in-order":
-        postOrder(data)
-        preOrder(data)
-    else:
-        print "Incorrect inputs.."
+    testList = [null] + testList
+    testList2 = [null] + testList2
+    testList3 = [null] + testList3
+    inOrder(testList3, 1)
 
 
-testList = [1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12]
 if __name__ == "__main__":
-    main(testList, "pre-order")
+    main()
