ed0abf28eb17248c6efb45361c257f2ab48646f0
Justin K -W10 <justinjoseph0007@gmail.com>
2017-03-21 08:48:44 -0400

    Finalised the program in a presentable way

--- src/traverse.py
+++ src/traverse.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python2.7
 import math
 
-# Assuming we have a complete binary tree
-
 result = []
 tempData = []
 
@@ -48,10 +46,11 @@ def inOrder(data, node):
 def preOrderToArray(data, node):
     global tempData
     global result
-    if node <= len(data) - 1:
+    if node <= len(data)-1:
         result[node] = tempData.pop(0)
         preOrderToArray(data, left(node))
         preOrderToArray(data, right(node))
+
 
 def postOrderToArray(data, node):
     global tempData
@@ -61,6 +60,7 @@ def postOrderToArray(data, node):
         postOrderToArray(data, right(node))
         result[node] = tempData.pop(0)
 
+
 def inOrderToArray(data, node):
     global tempData
     global result
@@ -68,10 +68,6 @@ def inOrderToArray(data, node):
         inOrderToArray(data, left(node))
         result[node] = tempData.pop(0)
         inOrderToArray(data, right(node))
-        
-def parent(i):
-    "Returns the index of the parent element of the child node at i"
-    return int(math.floor(i/2))
 
 
 def left(i):
@@ -84,53 +80,46 @@ def right(i):
     return (2*i)+1
 
 
-def swap(data, i, j):
-    "This method swaps the two points i and j in the list data"
-    data[i], data[j] = data[j], data[i]  # do THE swap
-    return data
-
-
-# def main(data, order):
-#     "This is the main method and the beginning of the program"
-#     null = None
-#     data = [null] + data  # Make the given array into a workable tree
-#     if order == "pre-order":
-#         inOrder(data)
-#         postOrder(data)
-#     elif order == "post-order":
-#         inOrder(data)
-#         preOrder(data)
-#     elif order == "in-order":
-#         postOrder(data)
-#         preOrder(data)
-#     else:
-#         print "Incorrect inputs.."
-
-# testList = [1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12]
-# if __name__ == "__main__":
-#     main(testList, "pre-order")
-
 def main():
+    "This is the main method and the beginning of the program"
+    print ""
     global result
     global tempData
-    print ""
-    testList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    testList2 = [1, 2, 9, 3, 6, 10, 12, 4, 5, 7, 8, 11]
-    testList3 = [1, 2, 3, 4, 5, 6, 7]
-    testList4 = [4, 5, 2, 3, 1]
-    tempData = [4, 5, 2, 3, 1]
-    null = None
-    testList = [null] + testList
-    testList2 = [null] + testList2
-    testList3 = [null] + testList3
-    testList4 = [null] + testList4
-    result = [null] * len(testList4)
-    # preOrderToArray(testList4, 1)
-    postOrderToArray(testList4, 1)
-    result.pop(0)
-    # preOrder(testList3, 1)
-    print result
-
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    tempData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    # tempData = list(data)
+    print "We have this array as our input"
+    print data
+    data = [None] + data  # Make the given array into a workable tree
+    result = [None] * len(data)
+    print "Please select the order of the above array by entering the corresponding number"
+    print "1. pre-order"
+    print "2. post-order"
+    print "3. in-order"
+    option = input()
+    if option == 1:
+        preOrderToArray(data, 1)
+        print "The post-order of the array is: ",
+        postOrder(result, 1)
+        print ""
+        print "The in-order of the array is: ",
+        inOrder(result, 1)
+    elif option == 2:
+        postOrderToArray(data, 1)
+        print "The in-order of the array is: ",
+        inOrder(result, 1)
+        print ""
+        print "The pre-order of the array is: ",
+        preOrder(result, 1)
+    elif option == 3:
+        inOrderToArray(data, 1)
+        print "The pre-order of the array is: ",
+        preOrder(result, 1)
+        print ""
+        print "The post-order of the array is: ",
+        postOrder(result, 1)
+    else:
+        print "Incorrect inputs.."
 
 if __name__ == "__main__":
     main()
