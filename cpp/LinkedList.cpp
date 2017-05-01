800158b686479cc67cbfc1efcf19bbfd6f45a812
Justin K -Linux <justinjoseph0007@gmail.com>
2017-05-01 19:14:32 -0400

    Broken,

--- cpp/LinkedList.cpp
+++ cpp/LinkedList.cpp
@@ -2,27 +2,46 @@
 
 using namespace std;
 
-struct node{
+struct Node{
   int data;
-  node *next;                   // This creates a node pointer named next
+  Node *next;                   // This creates a Node pointer named next
 };
 
 class LinkedList{
 public:
-  node *head;
-  node *tail;
-  node current;
+  Node *head;
+  Node *tail;
   void insertAtHead(int data);
-  LinkedList(){                 // Constructor
-    this->head = NULL;
-    this->tail = NULL;
-  };
+  void display();
+  // LinkedList(){              // Constructor
+  //   this->head = NULL;
+  //   this->tail = NULL;
+  // };
 };
 
-void LinkedList::insertAtHead(int data){
-  
+void LinkedList::insertAtHead(int element){
+  Node newNode;
+  newNode.data = element;
+  cout<<"Value of newNode now is "<<newNode.data<<endl;
+  newNode.next = head;
+  cout<<"Value of next node is "<<newNode.next<<endl;
+  head = &newNode;
+}
+
+void LinkedList::display(){
+  cout << "The contents of the list is\n";
+  Node *current = head;
+  while(current != tail){
+    cout << current->data<<endl;
+    current = current->next;
+  }
 }
 
 int main(){
   LinkedList myList;
+  myList.insertAtHead(1);
+  myList.insertAtHead(9);
+  myList.insertAtHead(34);
+  myList.insertAtHead(3);
+  // myList.display();
 }
