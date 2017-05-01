a0fbfd0b0fded7fb34e87f19f483a96339110670
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-01 19:34:46 -0400

    Removed the useless constructor

--- cpp/LinkedList.cpp
+++ cpp/LinkedList.cpp
@@ -10,28 +10,21 @@ struct Node{
 class LinkedList{
 public:
   Node *head;
-  Node *tail;
   void insertAtHead(int data);
   void display();
-  // LinkedList(){              // Constructor
-  //   this->head = NULL;
-  //   this->tail = NULL;
-  // };
 };
 
 void LinkedList::insertAtHead(int element){
   Node newNode;
   newNode.data = element;
-  cout<<"Value of newNode now is "<<newNode.data<<endl;
   newNode.next = head;
-  cout<<"Value of next node is "<<newNode.next<<endl;
   head = &newNode;
 }
 
 void LinkedList::display(){
   cout << "The contents of the list is\n";
   Node *current = head;
-  while(current != tail){
+  while(current->next != NULL){
     cout << current->data<<endl;
     current = current->next;
   }
@@ -43,5 +36,5 @@ int main(){
   myList.insertAtHead(9);
   myList.insertAtHead(34);
   myList.insertAtHead(3);
-  // myList.display();
+  myList.display();
 }
