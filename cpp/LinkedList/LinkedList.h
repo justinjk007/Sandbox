ac81c9b1b9bfe80eedc9df4a57dfe2f051a17a86
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-02 19:03:10 -0400

    Moved everything into the header file including defenitions

--- cpp/LinkedList/LinkedList.h
+++ cpp/LinkedList/LinkedList.h
@@ -1,21 +1,51 @@
-class LinkedList{		// This is the constructor, very important.
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <iostream>
+using namespace std;
+
+class LinkedList{		
  private:
   struct Node{
     int data;
-    Node* next;                   // This creates a Node pointer named next.
+    Node* next;                 // This creates a Node pointer named next.
   };
   int size;
   Node* head;
   Node* temp;
   Node* current;
  public:
-  LinkedList(){        
+  LinkedList(){                 // This is the constructor, very important.
     this->head = NULL;
     this->current = NULL;
     this->temp = NULL;
     size = 0;
   }
-  void insertAtHead(int data);
-  void display();
-  int getSize();
-};
+    void insertAtHead(int data);
+    void display();
+    int getSize();
+  };
+
+
+void LinkedList::insertAtHead(int element){
+  Node* newNode = new Node;
+  newNode->data = element;
+  size+=1;
+  temp = head;
+  head = newNode;
+  newNode->next = temp;
+}
+
+void LinkedList::display(){
+  cout << "The contents of the list is\n";
+  current = head;
+  for (int i = 1; i <= size; ++i) {
+    cout << current->data<<"\n";
+    current = current->next;
+  }
+}
+  
+int LinkedList::getSize(){
+  return size;
+}
+#endif /* LINKEDLIST_H */
