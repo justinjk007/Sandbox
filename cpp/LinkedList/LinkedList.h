d3c32795d11b7cc9916b425a3e3c534d394495d9
Justin K -Linux <justinjoseph0007@gmail.com>
2017-05-03 07:09:09 -0400

    Revert "Moved everything into the header file including defenitions"
    This reverts commit ac81c9b1b9bfe80eedc9df4a57dfe2f051a17a86.

--- cpp/LinkedList/LinkedList.h
+++ cpp/LinkedList/LinkedList.h
@@ -1,51 +1,21 @@
-#ifndef LINKEDLIST_H
-#define LINKEDLIST_H
-
-#include <iostream>
-using namespace std;
-
-class LinkedList{		
+class LinkedList{		// This is the constructor, very important.
  private:
   struct Node{
     int data;
-    Node* next;                 // This creates a Node pointer named next.
+    Node* next;                   // This creates a Node pointer named next.
   };
   int size;
   Node* head;
   Node* temp;
   Node* current;
  public:
-  LinkedList(){                 // This is the constructor, very important.
+  LinkedList(){        
     this->head = NULL;
     this->current = NULL;
     this->temp = NULL;
     size = 0;
   }
-    void insertAtHead(int data);
-    void display();
-    int getSize();
-  };
-
-
-void LinkedList::insertAtHead(int element){
-  Node* newNode = new Node;
-  newNode->data = element;
-  size+=1;
-  temp = head;
-  head = newNode;
-  newNode->next = temp;
-}
-
-void LinkedList::display(){
-  cout << "The contents of the list is\n";
-  current = head;
-  for (int i = 1; i <= size; ++i) {
-    cout << current->data<<"\n";
-    current = current->next;
-  }
-}
-  
-int LinkedList::getSize(){
-  return size;
-}
-#endif /* LINKEDLIST_H */
+  void insertAtHead(int data);
+  void display();
+  int getSize();
+};
