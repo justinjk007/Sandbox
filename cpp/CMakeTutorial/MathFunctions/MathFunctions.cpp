0d13e71aff303c91eddcf4b1d2cb09f9399c2a94
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-29 12:41:47 -0400

    Step 2 : Add libraries

---
+++ cpp/CMakeTutorial/MathFunctions/MathFunctions.cpp
@@ -0,0 +1,5 @@
+#include "MathFunctions.h"
+
+double mysqrt(double ques){
+  return 0909.0707;
+}
--- cpp/CMakeTutorial/tutorial.cpp
+++ cpp/CMakeTutorial/tutorial.cpp
@@ -4,23 +4,19 @@
 #include <stdlib.h>
 #include <math.h>
 #include "TutorialConfig.h"
+#include "MathFunctions.h"
 
 using namespace std;
 
-int main (int argc, char *argv[])
-{
-  if (argc < 2)
-    {
-      fprintf(stdout,"%s Version %d.%d\n",
-	      argv[0],
-	      Tutorial_VERSION_MAJOR,
-	      Tutorial_VERSION_MINOR);
-      fprintf(stdout,"Usage: %s number\n",argv[0]);
-      return 1;
-    }
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stdout, "%s Version %d.%d\n", argv[0], Tutorial_VERSION_MAJOR,
+            Tutorial_VERSION_MINOR);
+    fprintf(stdout, "Usage: %s number\n", argv[0]);
+    return 1;
+  }
   double inputValue = atof(argv[1]);
-  double outputValue = sqrt(inputValue);
-  fprintf(stdout,"The square root of %g is %g\n",
-          inputValue, outputValue);
+  double outputValue = mysqrt(inputValue);
+  fprintf(stdout, "The square root of %g is %g\n", inputValue, outputValue);
   return 0;
 }
