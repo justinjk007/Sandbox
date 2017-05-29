e75651c78f4e3563d47351095c1c808e0ea81b88
Justin K -W10 <justinjoseph0007@gmail.com>
2017-05-29 10:08:20 -0400

    Step 1

--- cpp/CMakeTutorial/tutorial.cpp
+++ cpp/CMakeTutorial/tutorial.cpp
@@ -3,13 +3,18 @@
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
+#include "TutorialConfig.h"
+
 using namespace std;
+
 int main (int argc, char *argv[])
 {
-  int n;
-  cin >> n;
   if (argc < 2)
     {
+      fprintf(stdout,"%s Version %d.%d\n",
+	      argv[0],
+	      Tutorial_VERSION_MAJOR,
+	      Tutorial_VERSION_MINOR);
       fprintf(stdout,"Usage: %s number\n",argv[0]);
       return 1;
     }
@@ -17,7 +22,5 @@ int main (int argc, char *argv[])
   double outputValue = sqrt(inputValue);
   fprintf(stdout,"The square root of %g is %g\n",
           inputValue, outputValue);
-  cout << "Hello World";
-
   return 0;
 }
