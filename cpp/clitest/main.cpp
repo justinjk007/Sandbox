e575acc93956e7b4343a36a717fd1e3fd7a5cc7b
Justin K -W10 <justinjoseph0007@gmail.com>
2017-06-30 13:10:39 -0400

    That was easy

---
+++ cpp/clitest/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Have " << argc << " arguments:" << std::endl;
+  for (int i = 0; i < argc; ++i) {
+    std::cout << argv[i] << std::endl;
+  }
+  return 0;
+}
