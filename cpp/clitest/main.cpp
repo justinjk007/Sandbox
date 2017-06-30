4b50103a2b4867c4517c9b6fc159717dca4efe33
Justin K -W10 <justinjoseph0007@gmail.com>
2017-06-30 18:58:37 -0400

    Added doc

--- cpp/clitest/main.cpp
+++ cpp/clitest/main.cpp
@@ -5,9 +5,18 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  std::cout << "Have " << argc << " arguments:" << std::endl;
-  for (int i = 0; i < argc; ++i) {
-    std::cout << argv[i] << std::endl;
+  if(argc < 2) {
+    printf("Test 1.0, the mini applet for Blah Blah\n");
+    printf("Copyright (c) 2017, JustinJK007.  All rights reserved.\n");
+    printf("This program was made available under the unlicense\n");
+    printf("See LICENSE file included with the distribution for details and disclaimer.\n");
+    printf("Error : No arguments specified (use -help or -h for help)\n");
+  }
+  else {
+    std::cout << "Have " << argc << " arguments:" << std::endl;
+    for (int i = 0; i < argc; ++i) {
+      std::cout << argv[i] << std::endl;
+    }
   }
   return 0;
 }
