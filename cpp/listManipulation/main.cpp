4f5dd4217e590905464756549e5e61c8e8d9c570
Justin K -Linux <justinjoseph0007@gmail.com>
2017-07-26 16:23:51 -0400

    I don't know whats the problem here
    I will figure this out when I get home. I am on a bus to home Oshawa
    now, ..btw who reads these.

---
+++ cpp/listManipulation/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+list<int> removeCommonints(list<int>);
+void print_list(list<int>);
+bool compareint(int line1, int line2);
+
+int main() {
+  cout << "Working\n";
+  list<int> testable = {45, 453, 46, 46, 47, 4, 1, 2};
+  testable = removeCommonints(testable);
+  return 0;
+}
+
+std::list<int> removeCommonints(std::list<int> lines) {
+  /**
+   * This method is used remove duplicate lines from the given line
+   * list, this will only for the specific case I have. If a duplicate
+   * is found both the original and the duplicate are removed from the
+   * list. This is done to eliminate inner line-segments of the
+   * translated structure.
+   */
+  std::list<int> new_lines;
+  bool flag = false;
+  while (!lines.empty()) {
+    int line1 = lines.front(); // Get the 1t line from the list
+    lines.pop_front();         // Remove the same line from the list
+    for (list<int>::iterator it = lines.begin(); it != lines.end(); it++) {
+      if (compareint(line1, *it)) { // Compare to see if same lines exists
+        lines.erase(it);            // Erase the duplicate if it exists
+        flag = true;
+      }
+    }
+    if (!flag) // If duplicate existed, don't push this.
+      new_lines.push_front(line1);
+  }
+  return new_lines;
+}
+
+void print_list(list<int> print_me) {
+  cout << "\n";
+  for (list<int>::iterator it = print_me.begin(); it != print_me.end(); ++it) {
+    cout << *it << "\n";
+  }
+}
+
+bool compareint(int line1, int line2) {
+  if (line1 == line2)
+    return true;
+  else
+    return false;
+}
