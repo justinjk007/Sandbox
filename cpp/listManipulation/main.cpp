72901d2b67dde842751640269db6eba234be1ee8
Justin K -W10 <justinjoseph0007@gmail.com>
2017-07-28 18:36:20 -0400

    Atleast there are no erros now

--- cpp/listManipulation/main.cpp
+++ cpp/listManipulation/main.cpp
@@ -8,10 +8,10 @@ void print_list(list<int>);
 bool compareint(int line1, int line2);
 
 int main() {
-  list<int> testable = {45, 453, 46, 46, 47, 4, 1, 2};
-  list<int> new_testable = removeCommonints(testable);
-  print_list(testable);
-  print_list(new_testable);
+  list<int> lines = {45, 453, 46, 46, 47, 4, 1, 2};
+  list<int> new_lines = removeCommonints(lines);
+  print_list(lines);
+  print_list(new_lines);
   return 0;
 }
 
@@ -23,23 +23,27 @@ std::list<int> removeCommonints(std::list<int> lines) {
    * list. This is done to eliminate inner line-segments of the
    * translated structure.
    */
-  std::list<int> lines_clone = lines;
   std::list<int> new_lines;
   bool flag = false;
+
   while (!lines.empty()) {
     int line1 = lines.front(); // Get the 1st line from the list
     lines.pop_front();         // Remove the same line from the list
     for (list<int>::iterator it = lines.begin(); it != lines.end(); ++it) {
-      // if (compareint(line1, *it)) { // Compare to see if same lines exists
-      if (*it == 46) {   // Compare to see if same lines exists
-        lines.erase(it); // Erase the duplicate if it exists
-        flag = true;     // Flag that duplicate exits
+      if (compareint(line1, *it)) { // Compare to see if same lines exists
+        if (!lines.empty()){
+          // lines.erase(it); // Erase the duplicate if it exists
+	  flag = true;       // Flag that duplicate exits
+	}
       }
+      else
+	;
     }
-    if (!flag) // If duplicate doesn't exist. push this to the list.
-      new_lines.push_front(line1);
-    flag = false; // Reset flag
+    // if (!flag) // If duplicate doesn't exist. push this to the list.
+    //   new_lines.push_front(line1);
+    // flag = false; // Reset flag
   }
+
   return new_lines;
 }
 
