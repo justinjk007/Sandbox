b077f3299d0e20058635e0ada3dcc218c8403d47
Justin K -W10 <justinjoseph0007@gmail.com>
2017-08-07 18:53:56 -0400

    Clang-format testing

--- cpp/fileIO/main.cpp
+++ cpp/fileIO/main.cpp
@@ -2,16 +2,17 @@
 
 using namespace std;
 
-int main(){
-  ofstream myfile;
-  // (5.09,5.80), (1.68,4.90), (1.48,1.38), (4.76,0.10), (7.00,2.83)
-  myfile.open ("example.csv");
-  myfile << "x,y,\n";
-  myfile << "5.09,5.80,\n";
-  myfile << "1.68,4.90\n";
-  myfile << "1.48,1.38\n";
-  myfile << "4.76,0.10\n";
-  myfile << "7.00,2.83\n";
-  myfile.close();
-  return 0;
+int main()
+{
+    ofstream myfile;
+    // (5.09,5.80), (1.68,4.90), (1.48,1.38), (4.76,0.10), (7.00,2.83)
+    myfile.open("example.csv");
+    myfile << "x,y,\n";
+    myfile << "5.09,5.80,\n";
+    myfile << "1.68,4.90\n";
+    myfile << "1.48,1.38\n";
+    myfile << "4.76,0.10\n";
+    myfile << "7.00,2.83\n";
+    myfile.close();
+    return 0;
 }
--- cpp/listManipulation/main.cpp
+++ cpp/listManipulation/main.cpp
@@ -8,66 +8,70 @@ void print_list(list<int>);
 bool compareint(int line1, int line2);
 std::list<int> removeint(std::list<int> lines, int val);
 
-int main() {
-  list<int> lines = {45, 453, 46, 47, 46, 4, 1, 2, 45};
-  list<int> new_lines = removeCommonints(lines);
-  print_list(lines);
-  print_list(new_lines);
-  return 0;
+int main()
+{
+    list<int> lines     = {45, 453, 46, 47, 46, 4, 1, 2, 45};
+    list<int> new_lines = removeCommonints(lines);
+    print_list(lines);
+    print_list(new_lines);
+    return 0;
 }
 
-std::list<int> removeCommonints(std::list<int> lines) {
-  /**
-   * This method is used remove duplicate lines from the given line
-   * list, this will only for the specific case I have. If a duplicate
-   * is found both the original and the duplicate are removed from the
-   * list. This is done to eliminate inner line-segments of the
-   * translated structure.
-   */
-  std::list<int> new_lines = lines;
+std::list<int> removeCommonints(std::list<int> lines)
+{
+    /**
+     * This method is used remove duplicate lines from the given line
+     * list, this will only for the specific case I have. If a duplicate
+     * is found both the original and the duplicate are removed from the
+     * list. This is done to eliminate inner line-segments of the
+     * translated structure.
+     */
+    std::list<int> new_lines = lines;
 
-  while (!lines.empty()) {
-    int line1 = lines.front(); // Get the 1st line from the list
-    lines.pop_front();         // Remove the same line from the list
-    for (list<int>::iterator it = lines.begin(); it != lines.end(); ++it)
-      if (compareint(line1, *it)) // Compare to see if same lines exists
-        new_lines = removeint(new_lines, line1); // new_lines.remove(line1);
-  }
+    while (!lines.empty()) {
+        int line1 = lines.front();  // Get the 1st line from the list
+        lines.pop_front();          // Remove the same line from the list
+        for (list<int>::iterator it = lines.begin(); it != lines.end(); ++it)
+            if (compareint(line1, *it))                   // Compare to see if same lines exists
+                new_lines = removeint(new_lines, line1);  // new_lines.remove(line1);
+    }
 
-  return new_lines;
+    return new_lines;
 }
 
-void print_list(list<int> print_me) {
-  cout << "(";
-  for (list<int>::iterator it = print_me.begin(); it != print_me.end();) {
-    cout << *it;
-    it++;
-    if (it != print_me.end())
-      cout << ", ";
-  }
-  cout << ")\n";
+void print_list(list<int> print_me)
+{
+    cout << "(";
+    for (list<int>::iterator it = print_me.begin(); it != print_me.end();) {
+        cout << *it;
+        it++;
+        if (it != print_me.end()) cout << ", ";
+    }
+    cout << ")\n";
 }
 
-bool compareint(int line1, int line2) {
-  /**
-   * Just to clarify we obviously don't need this in real life. This
-   * actually compares lines strcuts, this is just a test repo.
-   */
+bool compareint(int line1, int line2)
+{
+    /**
+     * Just to clarify we obviously don't need this in real life. This
+     * actually compares lines strcuts, this is just a test repo.
+     */
     if (line1 == line2)
-    return true;
-  else
-    return false;
+        return true;
+    else
+        return false;
 }
 
-std::list<int> removeint(std::list<int> lines, int val) {
-  /**
-   * Override function for list::remove rewritten for list<Line>
-   */
-  for (list<int>::iterator it = lines.begin(); it != lines.end();) {
-    if (compareint(val, *it)) // If same lines exists
-      it = lines.erase(it);
-    else
-      ++it;
-  }
-  return lines;
+std::list<int> removeint(std::list<int> lines, int val)
+{
+    /**
+     * Override function for list::remove rewritten for list<Line>
+     */
+    for (list<int>::iterator it = lines.begin(); it != lines.end();) {
+        if (compareint(val, *it))  // If same lines exists
+            it = lines.erase(it);
+        else
+            ++it;
+    }
+    return lines;
 }
