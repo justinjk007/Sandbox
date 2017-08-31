49819dc924ab4287f17fa4402e2dc6c8dc401037
Justin K -W10 <justinjoseph0007@gmail.com>
2017-08-30 21:31:33 -0400

    Better strcuture

--- cpp/imgui/main.cpp
+++ cpp/imgui/main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
-#include "imgui.h"
-#include "imgui_internal.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
 
 using namespace std;
 using namespace ImGui;
 
 int main()
 {
-    Text("Hello, world %d", 123);
 
-    if (Button("OK?")) {
-        // do stuff
-    }
+  cout << "Hello World";
+  // Text("Hello, world %d", 123);
 
-    InputText("string", buf, 256);
-    SliderFloat("float", &f, 0.0f, 1.0f);
-    return 0;
+  // if (Button("OK?")) {
+  //   // do stuff
+  // }
+
+  // InputText("string", buf, 256);
+  // SliderFloat("float", &f, 0.0f, 1.0f);
+  return 0;
 }
