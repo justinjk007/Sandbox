587f268680761cd8c238e2e4956c10b151957344
Justin K -W10 <justinjoseph0007@gmail.com>
2017-09-15 14:19:29 -0400

    Sort 100,000 random elements and time it -> 1.5 seconds rougly

--- cpp/Sorting/main.cpp
+++ cpp/Sorting/main.cpp
@@ -1,45 +1,24 @@
-#include <algorithm>  // For the sort function
-#include <cstdlib>    // For the rand() function
-#include <ctime>      // For the time function
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <list>
+#include <chrono>
 
 using namespace std;
 
-struct Point {
-  double x;
-  double y;
-  double slope;
-  Point() {
-    this->x = 0.0;
-    this->y = 0.0;
-    this->slope = 0.0;
-  }
-};
-
-bool comparePoint(Point, Point);
-
-int main()
+int main( )
 {
-    list<Point> points;
-    srand((unsigned int)time(NULL));  // use current time as seed for random generator
-    for (int i = 0; i < 10; ++i) {
-        int random_var = rand();
-        Point a;
-        a.slope = i * 1.0 * random_var;
-        points.push_back(a);
-    }
-    points.sort(comparePoint);
-    for (list<Point>::iterator it = points.begin(); it != points.end(); ++it) {
-        cout << "-- " << it->slope << " --\n";
-    }
-}
 
-bool comparePoint(Point a,Point b)
-{
-    /**
-     * Compare a return the point with the biggest slope, in boolean
-     * values
-     */
-    return (a.slope > b.slope);
+    auto start = chrono::high_resolution_clock::now();  // Start clock
+
+    list<int> data( 100000 );
+    generate( data.begin(), data.end(), rand ); // Populate with random numbers
+    data.sort();
+
+    auto finish                      = chrono::high_resolution_clock::now();  // Stop clock
+    chrono::duration<double> elapsed = finish - start;                        // Mesure time elapsed
+    cout << "\n------ Elapsed time: " << elapsed.count() << " s ------\n";
+
+    return 0;
 }
