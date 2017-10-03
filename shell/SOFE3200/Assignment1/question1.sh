57fd141e6b9f66cada853b5146de7d58783f3bd4
Justin K -School <justinjoseph0007@gmail.com>
2017-10-03 08:16:34 -0400

    Better formatting

--- shell/SOFE3200/Assignment1/question1.sh
+++ shell/SOFE3200/Assignment1/question1.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
 # Get the input from the user
 echo "Enter a sample input:"
 read -e sample_text
 echo "Enter substring to search:"
 read -e sub_string
 
-echo "Number of occurrences of \"$sub_string\" in \"$sample_text\" is"
 # Use grep to search for the substring, -o option is used return only
 # matching lines Then pipe the output of grep into wc, the -l flag is
 # used to output only line count which is the same as the number of
 # times the substring appeared in the sample text.
-echo "$sample_text" | grep -o "$sub_string" | wc -l
+ans=$(echo "$sample_text" | grep -o "$sub_string" | wc -l)
+
+echo "Number of occurrences of \"$sub_string\" in \"$sample_text\" is $ans"
