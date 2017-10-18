b934672038a69eaa8ff3266139f20356b9b8d40f
Justin K -School <justinjoseph0007@gmail.com>
2017-10-17 21:36:57 -0400

    You gotta start somewhere

---
+++ web/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[EmployeeID] INT NOT NULL PRIMARY KEY, 
+    [EmployeeName] NVARCHAR(MAX) NULL, 
+    [Department] INT NULL, 
+    [Telephone] NVARCHAR(MAX) NULL, 
+    [Extension] NVARCHAR(50) NULL, 
+    [Title] NVARCHAR(MAX) NULL, 
+    [Email] NVARCHAR(MAX) NULL,
+    CONSTRAINT [fk_department] FOREIGN KEY ([Department]) REFERENCES [Departments]([DepartmentID]) ON UPDATE CASCADE
+)
