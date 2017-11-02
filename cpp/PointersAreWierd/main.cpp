c3a0dbb86eb6ef494898874b73f3d029cf848feb
Justin K -W10 <justinjoseph0007@gmail.com>
2017-11-01 22:44:23 -0400

    They taught me pointers wrong...now I know better

--- cpp/PointersAreWierd/main.cpp
+++ cpp/PointersAreWierd/main.cpp
@@ -8,7 +8,14 @@ int main()
     double* double_pointer       = &val;
     double val_array[3]          = {2, 3, 5};
     double* double_array_pointer = &val_array[0];
+
+    // This works, both loops are the same thing
     for (int i = 0; i < 3; ++i) cout << double_array_pointer[i] << "\n";
+    for (int i = 0; i < 3; ++i) cout << *(double_array_pointer + i) << "\n";
     // This also works for vectors
+
+    // These are the same, weirdly we are dereferencing a varible pointer like an array
+    cout << double_pointer[0] << "\n";
+    cout << *(double_pointer + 0) << "\n";
     return 0;
 }
