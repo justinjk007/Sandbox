d6231b1cb9f1eb4b278d3a265a47af9f78900e91
Justin K -W10 <justinjoseph0007@gmail.com>
2017-11-09 21:39:05 -0500

    I don't really get this but...ok

--- java/Parallel.java
+++ java/Parallel.java
@@ -1,9 +1,26 @@
 import java.util.*;
 
-public class Parallel
+public class Parallel extends Thread
 {
-    public static void main(String[] args)
-    {
-	System.out.println("Hello world");
+    public static int x =1;	    // Global variable
+    public static boolean flag = true; // Semaphore
+    public void run(){
+	if(flag)
+	    {
+		++x;
+		flag = false; // This thread has been already been ran once
+		System.out.println("Thread ran..");
+	    }
+	else
+	    System.out.println("Thread prevented..");
+    }
+    public static void main(String args[]){
+	Parallel thread1 = new Parallel();
+	thread1.start();
+
+	Parallel thread2 = new Parallel();
+	thread2.start();
+
+	System.out.print("X after running two threads safely: "+ x + "\n");
     }
 }
