510c18f1b02b37409d357dd2536a7cafd43da8bf
Justin K -W10 <justinjoseph0007@gmail.com>
2018-03-07 19:33:28 -0500

    Initial code commit

---
+++ cpp/Software Quality/Assignment 2/src/helper.cpp
@@ -0,0 +1,48 @@
+#include "helper.hpp"
+#include <iostream>
+
+using namespace std;
+
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+        return 1;
+    else if (square[4] == square[5] && square[5] == square[6])
+        return 1;
+    else if (square[7] == square[8] && square[8] == square[9])
+        return 1;
+    else if (square[1] == square[4] && square[4] == square[7])
+        return 1;
+    else if (square[2] == square[5] && square[5] == square[8])
+        return 1;
+    else if (square[3] == square[6] && square[6] == square[9])
+        return 1;
+    else if (square[1] == square[5] && square[5] == square[9])
+        return 1;
+    else if (square[3] == square[5] && square[5] == square[7])
+        return 1;
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' &&
+             square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' &&
+             square[9] != '9')
+        return 0;
+    else
+        return -1;
+}
+
+void displayBoard()
+{
+    system("cls");  // Clear old board
+    cout << "\tTic Tac Toe";
+    cout << "Player 1 marks with X \nPlayer 2 marks with O\n\n";
+    cout << "     |     |     \n";
+    cout << "  " << square[1] << "  |  " << square[2] << "  |  " << square[3] << "\n";
+    cout << "_____|_____|_____\n";
+    cout << "     |     |     \n";
+    cout << "  " << square[4] << "  |  " << square[5] << "  |  " << square[6] << "\n";
+    cout << "_____|_____|_____"
+         << "\n";
+    cout << "     |     |     "
+         << "\n";
+    cout << "  " << square[7] << "  |  " << square[8] << "  |  " << square[9] << "\n";
+    cout << "     |     |     \n\n\n";
+}
---
+++ cpp/Software Quality/Assignment 2/src/helper.hpp
@@ -0,0 +1,9 @@
+#ifndef HELPER_HPP
+#define HELPER_HPP
+
+char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+int checkwin();
+void displayBoard();
+
+#endif /* HELPER_HPP */
