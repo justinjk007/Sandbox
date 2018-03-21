c57a53870b307f2ca7eed77dffa3098c0b006ac0
Justin K -Linux <justinjoseph0007@gmail.com>
2018-03-21 13:06:33 -0400

    A queue that works

--- C/Queue/main.c
+++ C/Queue/main.c
@@ -4,10 +4,26 @@
 int main()
 {
     struct node* queue = NULL;
-    queue              = enqueue(queue, 2);
-    queue              = enqueue(queue, 3);
-    queue              = enqueue(queue, 4);
-    queue              = enqueue(queue, 5);
+    struct node* temp  = NULL;
+
+    temp = enqueue(queue, 2);
+    if (queue == NULL) queue = temp;
+    temp = enqueue(queue, 3);
+    if (queue == NULL) queue = temp;
+    temp = enqueue(queue, 4);
+    if (queue == NULL) queue = temp;
+    temp = enqueue(queue, 5);
+    if (queue == NULL) queue = temp;
+
+    temp  = queue;
+    while (temp != NULL) {
+        printf("%d\n", temp->value);
+        temp = temp->next;
+    }
+
+    queue = dequeue(queue);
+    queue = dequeue(queue);
+
     while (queue != NULL) {
         printf("%d\n", queue->value);
         queue = queue->next;
