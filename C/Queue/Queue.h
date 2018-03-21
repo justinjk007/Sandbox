1789d68082b58189431873e2ec33af180de88e6a
Justin K -Linux <justinjoseph0007@gmail.com>
2018-03-21 12:17:06 -0400

    A queue that still won't work

--- C/Queue/Queue.h
+++ C/Queue/Queue.h
@@ -6,30 +6,26 @@
 typedef struct Queue {
     int data;
     struct Queue* next;
-    struct Queue* previous;
 } Queue;
 
 typedef Queue* Node;
-Node head = NULL;
-Node tail = NULL;
 
-Node createNode(){
+Node createNode()
+{
     Node temp;
-    temp = (Node)malloc(sizeof(Queue));
+    temp       = (Node)malloc(sizeof(Queue));
     temp->next = NULL;
-    temp->previous = NULL;
     return temp;
 }
 
-void enqueue(int val){
+Node enqueue(Node head, int val)
+{
     Node temp;
-    temp = createNode();
+    temp       = createNode();
     temp->data = val;
-    if(head != NULL)
-	temp->next = head;
-    if(tail == NULL)
-	tail = temp;
+    temp = head;
     head = temp;
+    return head;
 }
 
 #endif /* QUEUE_HPP */
--- C/Queue/main.c
+++ C/Queue/main.c
@@ -4,5 +4,14 @@
 int main()
 {
     printf("Ellooo\n");
+    Queue * test;
+    enqueue(test,2);
+    enqueue(test,2);
+    enqueue(test,2);
+    enqueue(test,2);
+    while(test == NULL){
+	printf("%d",test->data);
+	test = test->next;
+    }
     return 0;
 }
