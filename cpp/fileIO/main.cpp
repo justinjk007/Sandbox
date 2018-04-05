a119043a36a92681dbf32aaeb52b9267365ed2db
Justin K -W10 <justinjoseph0007@gmail.com>
2018-04-04 20:59:22 -0400

    Correct way of reading csv file using getline

--- cpp/fileIO/main.cpp
+++ cpp/fileIO/main.cpp
@@ -1,18 +1,36 @@
 #include <fstream>
-
-using namespace std;
+#include <iostream>
+#include <string>
 
 int main()
 {
-    ofstream myfile;
-    // (5.09,5.80), (1.68,4.90), (1.48,1.38), (4.76,0.10), (7.00,2.83)
-    myfile.open("example.csv");
-    myfile << "x,y,\n";
-    myfile << "5.09,5.80,\n";
-    myfile << "1.68,4.90\n";
-    myfile << "1.48,1.38\n";
-    myfile << "4.76,0.10\n";
-    myfile << "7.00,2.83\n";
-    myfile.close();
-    return 0;
+    // File for input
+    std::ifstream file;
+    file.open("FooBar.csv", std::ios::in);
+    if (!file) {
+        std::cerr << "Unable to open csv file";
+        exit(1);
+    }
+
+    double sum1[3] = {0.0};
+    double sum2[3] = {0.0};
+    int scenario   = 0;
+    std::string cell;
+
+    while (getline(file, cell, ',')) {
+        scenario = std::stoi(cell);
+        for (int skip = 0; skip < 7; skip++) {
+            getline(file, cell, ',');  // Skip next 7 columns
+        }
+        getline(file, cell, ',');
+        sum1[scenario - 1] += stod(cell);
+        getline(file, cell, ',');  // Skip 1 more column
+        getline(file, cell, '\n');
+        sum2[scenario - 1] += stod(cell);
+    }
+    std::cout << 1 << "," << sum1[0] / 125.0 << "," << sum2[0] / 125.0 << "\n";
+    std::cout << 2 << "," << sum1[1] / 125.0 << "," << sum2[1] / 125.0 << "\n";
+    std::cout << 3 << "," << sum1[2] / 125.0 << "," << sum2[2] / 125.0 << "\n";
+
+    file.close();
 }
