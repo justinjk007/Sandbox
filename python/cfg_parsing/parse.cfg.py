fe8ec5e303ef66577a66c05b4adebeae4d56e3d8
Justin K -Work <Justin.Kaipada@ibm.com>
2018-05-28 08:48:11 -0400

    Testing py yapf buffer

--- python/cfg_parsing/parse.cfg.py
+++ python/cfg_parsing/parse.cfg.py
@@ -6,9 +6,9 @@ import ConfigParser
 config_contents = ConfigParser.RawConfigParser()
 config_contents.readfp(open("./config.cfg"))
 
-dictionary1 = dict() # Create empty dictionary
-dictionary1["name"] = config_contents.get("Some Section name","some_id")
-dictionary1["number1"] = config_contents.get("Some Section name","num1")
-dictionary1["number2"] = config_contents.get("Some Section name","num2")
+dictionary1 = dict()  # Create empty dictionary
+dictionary1["name"] = config_contents.get("Some Section name", "some_id")
+dictionary1["number1"] = config_contents.get("Some Section name", "num1")
+dictionary1["number2"] = config_contents.get("Some Section name", "num2")
 
 print dictionary1
--- python/plot_lines.py
+++ python/plot_lines.py
@@ -7,22 +7,24 @@ import matplotlib.pyplot as plt
 def parse(file_name):
     "Parses the data sets from the csv file we are given to work with"
     file = open(file_name)
-    rawFile = csv.reader(file)    # Reading the csv file into a raw form
-    rawData = list(rawFile)       # Converting the raw data into list from.
+    rawFile = csv.reader(file)  # Reading the csv file into a raw form
+    rawData = list(rawFile)  # Converting the raw data into list from.
     file.close()
     return rawData
+
 
 def draw(xCords, yCords, xLabel, yLabel, filename):
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
-    plt.title("Pentagon - "+filename)
+    plt.title("Pentagon - " + filename)
     plt.plot(xCords, yCords, '-o', color="r")
+
 
 def toXandY(unorderedData):
     "This method converts seperates x and y co-ordinates for plotting"
     orderedData = []
-    orderedData.append([])        # Add a new sublist every time
-    orderedData.append([])        # Add a new sublist every time
+    orderedData.append([])  # Add a new sublist every time
+    orderedData.append([])  # Add a new sublist every time
     listSize = len(unorderedData)
     for x in range(0, listSize):
         orderedData[0].append(unorderedData[x][0])  # Seperates the x-cords
@@ -30,18 +32,19 @@ def toXandY(unorderedData):
         orderedData[1].append(unorderedData[y][1])  # Seperates the y-cords
     return orderedData
 
+
 def main():
     newData = []
     file_name = "example.csv"
-    data = parse(file_name)   # Calling the parse funtion we made
-    labels = data.pop(0)            # Necessary evil
+    data = parse(file_name)  # Calling the parse funtion we made
+    labels = data.pop(0)  # Necessary evil
     listSize = len(data)
-    for i in range(0, listSize):    # Converting the string list to float
-        newData.append([])          # Add a new sublsit every time
-        for j in range(0, 2):       # Append converted data to the new list
+    for i in range(0, listSize):  # Converting the string list to float
+        newData.append([])  # Add a new sublsit every time
+        for j in range(0, 2):  # Append converted data to the new list
             newData[i].append(float(data[i][j]))
             # Finding the starting point P
-    DataXandY = toXandY(newData)     # DataXandY -> [[Xs][Ys]]
+    DataXandY = toXandY(newData)  # DataXandY -> [[Xs][Ys]]
     draw(DataXandY[0], DataXandY[1], labels[0], labels[1], file_name)
     plt.show()
 
