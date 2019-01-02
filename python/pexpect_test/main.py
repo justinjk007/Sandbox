96f351eb5a2f0057195836b8c2d32b94f4f21a6e
Justin K -Mac <justinjoseph0007@gmail.com>
2019-01-02 14:05:02 -0500

    Can change passwds using this expect script now

--- python/pexpect_test/main.py
+++ python/pexpect_test/main.py
@@ -8,7 +8,17 @@ child = pexpect.spawn('bash')
 child.logfile = sys.stdout
 child.expect('\$')
 child.sendline('ssh '+secret.machine)
-child.expect('password: ')
+child.expect('password:')
 child.sendline(secret.password)
+child.expect('\$')
+child.sendline('passwd')
+child.expect('password:') # Expect old passwd line
+child.sendline(secret.password)
+child.expect('password:') # Expect new passwd line
+child.sendline(secret.new_password)
+child.expect('password again:') # Expect new passwd again line
+child.sendline(secret.new_password)
+child.expect('\$')
+child.sendline('ls -l')
 child.expect('\$')
 child.close()
