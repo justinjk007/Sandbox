d4031f42563c44f77355645c72c0a18767ed992d
Justin K -Mac <justinjoseph0007@gmail.com>
2019-01-13 21:06:26 -0500

    Can format time now

--- python/Peel Region Waste Collection Cal/main.py
+++ python/Peel Region Waste Collection Cal/main.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 # from ics import Calendar, Event
+
 
 # c = Calendar()
 # e = Event()
@@ -11,6 +13,8 @@
 
 file_data = list() # Raw file data
 dates_dictionary = dict() # Dictionary with Dates and number of entries correspinding to each day
+description = list() # Store the description of events happening each day
+YEAR = '2019'
 
 def parse_file(fname):
     with open(fname) as f:
@@ -21,13 +25,27 @@ def parse_file(fname):
 
 def main():
     file_data = parse_file("raw_data.txt")
-    # If the last char of a line is a number it is date
+
+    # Parse data into Dates and events
+    date = ''
+    event_num = 0
     for x in file_data :
+        # If the last char of a line is a number it is date
         x_code = unicode(x, 'utf-8')
         if x_code[-1:].isnumeric():
-            dates_dictionary.update({x:0})
+            date = x
+            event_num = 0
+            dates_dictionary.update({date:event_num})
+        else:
+            event_num += 1
+            dates_dictionary.update({date:event_num})
+            description.append(x) # This is a waste type, not a date
+    # Convert dates into needed format
+    # https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
+    date = datetime.strptime('Monday, February 4'+' '+YEAR, '%A, %B %d %Y') # Strip date
+    print date.strftime('%Y%m%d') # Format date from stripped date
 
-    print dates_dictionary
+    
 
 
 if __name__ == "__main__":
