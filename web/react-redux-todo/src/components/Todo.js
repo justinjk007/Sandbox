4e2646287926374249a443fcc8e11985c2c53694
Justin Kaipada <justinjoseph0007@gmail.com>
2021-02-02 21:38:49 -0500

    TODOs can be flipped on an off

--- web/react-redux-todo/src/components/Todo.js
+++ web/react-redux-todo/src/components/Todo.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { toggleTodo } from '../redux/actions';
 import cx from 'classnames';
 
-const Todo = ({ todo }) => (
-  <li className='todo-item' onClick={() => {} /** dispatches action to toggle todo */}>
-    {todo && todo.completed ? '👌' : '👋'}{' '}
+const Todo = ({ todo, toggleTodo }) => (
+  <li className='todo-item' onClick={() => toggleTodo(todo.id)}>
+    <span className={cx('todo-item__tag', todo && todo.completed && 'todo-item__tag--completed')}>
+      {todo && todo.completed ? 'DONE:' : 'TODO:'} {"  "/*This just adds whites space*/}
+    </span>
     <span className={cx('todo-item__text', todo && todo.completed && 'todo-item__text--completed')}>
       {todo.content}
     </span>
   </li>
 );
 
-export default Todo;
+export default connect(
+  null, // mapStateToProps
+  { toggleTodo } // mapDispatchToProps
+)(Todo); // component name
