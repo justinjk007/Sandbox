60d0aab5c1875373d89d153dd6ec5c663bfdf430
Justin K -W10 <justinjoseph0007@gmail.com>
2022-03-06 22:05:59 -0500

    New songs playlist to listen to all songs in new Album
    Donda 2 for example

--- python/YoutubeMusicManager/main.py
+++ python/YoutubeMusicManager/main.py
@@ -5,7 +5,10 @@ ytmusic = YTMusic('headers_auth.json')
 ## automatically add recently added songs to the "All songs" playlist
 ## so it can be downloaded in one go on the phone
 
-ALL_SONGS_PLAYLIST = 'PLqILQSyq0LeiWwiGHpgI3NO_aNbTpDhoX' #get the playlist ID from the URL
+#get the playlist ID from the URL
+PLAYLIST = 'PLqILQSyq0LeiWwiGHpgI3NO_aNbTpDhoX' # All songs
+# PLAYLIST = 'PLqILQSyq0LehIneD7MdGhpdxoLfe61xiK' # New Songs
+
 if(len(sys.argv) > 1):
     HOW_MANY_SONGS = int(sys.argv[1]) # use cmd arg if we pass it
 else:
@@ -21,5 +24,5 @@ for i in range(0,HOW_MANY_SONGS):
     print("\t","Title:",item["title"]," ID:",item["videoId"])
 
 #  https://ytmusicapi.readthedocs.io/en/latest/reference.html#ytmusicapi.YTMusic.add_playlist_items
-result = ytmusic.add_playlist_items(ALL_SONGS_PLAYLIST, new_ids_to_be_added , None, False)
+result = ytmusic.add_playlist_items(PLAYLIST, new_ids_to_be_added , None, False)
 print("Status: ", result)
